4065a3809220f47f253da0ed284308cc29299dd6
CAZOTMCKO <859035546@qq.com>
2025-03-19 17:29:16 +0800

    添加事务的全局变量

---
+++ app/trancation.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"sync"
+)
+
+// type Multicmd struct {
+//     isInMulti bool
+//     queue     [][]string
+// }
+
+// var Multicmds = make(map[string]*Multicmd)
+// var mu sync.Mutex
+
+// 线程安全锁
+var mu sync.Mutex
+
+// 事务状态
+var isInTransaction bool = false
+var transactionQueue [][]string // 存储事务中的命令和参数
